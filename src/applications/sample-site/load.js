9ffdfc2b77d17504a0f7de3169d0e1b6b9bc1e69
Lea de Groot <l.degroot@uq.edu.au>
2021-01-21 08:23:34 +1000

    first try at a dummy site that is inserting header [PT #176547487]

--- src/applications/sample-site/load.js
+++ src/applications/sample-site/load.js
@@ -13,24 +13,24 @@ function loadReusableComponents() {
     const firstElement = document.body.children[0];
 
     // insert alerts after body-tag
-    let alerts = document.querySelector('uql-alerts');
-    if (!alerts) {
-        // as a back up insert header if it's not defined already
-        alerts = document.createElement('uqlibrary-alerts');
-        document.body.insertBefore(alerts, firstElement);
-    }
+    // let alerts = document.querySelector('uql-alerts');
+    // if (!alerts) {
+    //     // as a back up insert header if it's not defined already
+    //     alerts = document.createElement('uqlibrary-alerts');
+    //     document.body.insertBefore(alerts, firstElement);
+    // }
 
     // insert header after alerts
     const header = document.createElement('uql-header');
     document.body.insertBefore(header, firstElement);
 
-    // insert sub footer before body-tag
-    const subFooter = document.createElement('uql-connect-footer');
-    document.body.appendChild(subFooter);
-
-    // insert footer before body-tag
-    const footer = document.createElement('uql-minimal-footer');
-    document.body.appendChild(footer);
+    // // insert sub footer before body-tag
+    // const subFooter = document.createElement('uql-connect-footer');
+    // document.body.appendChild(subFooter);
+    //
+    // // insert footer before body-tag
+    // const footer = document.createElement('uql-minimal-footer');
+    // document.body.appendChild(footer);
 
     window.addEventListener('WebComponentsReady', function () {
         // when react is ready - configure elements
