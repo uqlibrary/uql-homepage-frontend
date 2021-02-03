7fb77d5ad299d411e764f09cb94cb8f3faf974be
Lea de Groot <l.degroot@uq.edu.au>
2021-02-03 11:44:04 +1000

    alerts showing, but broken [PT #176547487]

--- src/modules/WebComponents/UqlAlerts.js
+++ src/modules/WebComponents/UqlAlerts.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as retargetEvents from 'react-shadow-dom-retarget-events';
+import JssProvider from 'react-jss/lib/JssProvider';
+
 import { Alert } from 'modules/SharedComponents/Toolbox/Alert/components/Alert';
+import { mui1theme } from 'config/index';
+
+import { createGenerateClassName } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+
+const generateClassName = createGenerateClassName({
+    dangerouslyUseGlobalCSS: false,
+    productionPrefix: 'uq-espace-',
+});
 
 export default class UqlAlerts extends HTMLElement {
     createUqlAlerts() {
-        return React.createElement(Alert, {}, React.createElement('slot'));
+        const alerts = React.createElement(Alert, {}, React.createElement('slot'));
+        const muithemeprovider = React.createElement(MuiThemeProvider, { theme: mui1theme }, alerts);
+        const themeprovider = React.createElement(ThemeProvider, { theme: mui1theme }, muithemeprovider);
+        return React.createElement(JssProvider, { generateClassName: generateClassName }, themeprovider);
     }
 
     connectedCallback() {
--- src/modules/WebComponents/index.js
+++ src/modules/WebComponents/index.js
@@ -1,4 +1,4 @@
 export { default as UqlAlerts } from './UqlAlerts';
-// export { default as UqlHeader } from './UqlHeader';
+export { default as UqlHeader } from './UqlHeader';
 export { default as UqHeader } from './UqHeader';
 export { default as UqMinimalFooter } from './UqMinimalFooter';
