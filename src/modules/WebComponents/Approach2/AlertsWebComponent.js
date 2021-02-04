b4183a558969ba16c46d8c0481b6142aa22742f3
Lea de Groot <l.degroot@uq.edu.au>
2021-02-05 07:38:23 +1000

    dump unused web component approach [PT #176547487]

--- src/modules/WebComponents/Approach2/AlertsWebComponent.js
+++
@@ -1,4 +0,0 @@
-import { ReactCustomElement } from 'web-components-with-react';
-import { Alert } from 'modules/SharedComponents/Toolbox/Alert';
-
-customElements.define('alerts-web-component', ReactCustomElement(Alert));
--- src/modules/WebComponents/Approach2/ConnectFooterWebComponent.js
+++
@@ -1,4 +0,0 @@
-import { ReactCustomElement } from 'web-components-with-react';
-import { ConnectFooter } from 'modules/SharedComponents/Footer';
-
-customElements.define('library-connect-footer', ReactCustomElement(ConnectFooter));
